3f16b7b4b989ea90fa6d407ed4e7bfe22585d4f7
samragi99 <143216421+samragi99@users.noreply.github.com>
2023-12-08 12:19:07 -0600

    create wishlist icon for all prodcuts item

--- src/pages/Product/Product.tsx
+++ src/pages/Product/Product.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Button from "../../Buttun/Button";
 import MyButton from "../../Buttun/MyButton";
+import { Link } from "react-router-dom";
 
 function Product() {
   const [product, setProduct] = useState([]);
@@ -17,9 +18,16 @@ function Product() {
           <div className="  flex justify-evenly gap-[20px]  flex-wrap items-center">
             {product.map((item) => (
               <div key={item.id} className=" w-[300px] border p-3 shadow-2xl">
-                <h2 className="font-[800] text-amber-800 text-[20px] uppercase">
-                  {item.category}
-                </h2>
+                <div className="flex justify-between items-center py-3">
+                  <h2 className="font-[800] text-orange-300 text-[20px] uppercase">
+                    {item.category}
+                  </h2>
+                  <p className="  text-[#AB192B] ">
+                    <a href="">
+                      <i className="bi bi-bag-heart-fill" />
+                    </a>
+                  </p>
+                </div>
                 <img src={item.thumbnail} className="w-full h-[200px]" alt="" />
                 <div className="flex justify-between py-2">
                   <p>{item.brand}</p>
